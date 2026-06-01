b61a7d9471e1f2cccd2a0408aa4be2c5b3d42a76
JulioSteve <jules.stevenot25@gmail.com>
2026-06-01 15:23:19 +0200

    up

--- LinQT(HOMODYNE)/harm.jl
+++ LinQT(HOMODYNE)/harm.jl
@@ -1,14 +1,13 @@
-using Plots, LaTeXStrings, QuantumToolbox
+ENV["GKSwstype"] = "100"
+using QuantumToolbox, Plots, LaTeXStrings, Printf
 theme(:dao)
 palette = theme_palette(:dao)
 
 function N_BE(x)
-    if x == 0
-        return 0
-    elseif x>0
-        return 1/(exp(x)-1)
+    if x == -1
+        return 0 # χ→+∞ ≡ T→0 ⇒ N_th→0
     else
-        throw(ArgumentError("x must be positive, you have written x=$x.")) 
+        return 1/(exp(x)-1)
     end
 end
 
@@ -16,18 +15,19 @@ println("---------------------")
 
     # Simulation Parameters
 # ===========================================
-# Ntraj = 3 # number of trajectories
 α0 = 2.0 # Starting level for the initial coherent state
-χ = 0 # ħω/k_BT - if it's 0 it inputs the 0-temperature case, if positive it uses the Bose-Einstein distribution
-    # Physical Parameters
-# ===========================================
-Nth = N_BE(χ) # initial mean thermal photon number in the cavity 
+χ = 5
+Nth = N_BE(χ) # mean thermal photon number in the cavity 
 tlist = range(0,10,1000)
-Ncut = Nth + 4*√(Nth*(1+Nth)) + abs(α0)^2+4*abs(α0) 
-# |-> Dimension cutoff in the operators: μ+4σ of thermal state (steady state) + μ+4σ of initial coherent state
-Ncut = Int(ceil(Ncut*1.5)) # We take +50% of the maximal population and round it to the upper value to be safe
-println("⚠ Nth≈$(Int(ceil(Nth))) => Ncut=$Ncut")
 ω_k = 1.0 # unitless frequency system oscillation / coupling ratio (weak coupling regime if ω_k>>1)
+
+
+Ncut = Nth + 4*√(Nth*(1+Nth)) + abs(α0)^2+4*abs(α0) # Dimension cutoff in the operators: μ+4σ of thermal state (steady state) + μ+4σ of initial coherent state
+
+prct = 100 # POURCENTAGE DE TRONCATURE EN PLUS
+factor = 1.0+prct/100
+Ncut = Int(ceil(Ncut*factor)) # We take +prct% of the maximal population and round it to the upper value
+# println("⚠ Nth≈$(Int(ceil(Nth))) => Ncut=$Ncut")
 
 # Operators
 a = destroy(Ncut)
@@ -40,20 +40,8 @@ c_ops = [c_decay, c_excite]
 
 n_op = a'*a
 e_ops=[n_op]
-p = plot(title=L"ħω=%$(χ)k_{B}T \qquad N_{cut}=%$(Ncut)", size=(1920,1080), legend=:topright, titlefont=20, xtickfont=11, ytickfont=11, legendfontsize=20);
+H = ω_k*a'*a 
 
-H = ω_k*a'*a
-sol = mesolve(H, ψ0, tlist, c_ops; e_ops=e_ops, progress_bar=Val(true))
-Y = real.(sol.expect[1,:])
-plot!(p, tlist, Y,label=L"ω/k=%$(ω_k)",lw=10, color=palette[1]);
-n(t) = α0^2*exp(-t)+Nth*(1-exp(-t))
-N = n.(tlist)
-plot!(p, tlist, N, lw=3, label="model", color=palette[2]);
 
-Y2 = abs.(N.-Y)
-ytickslog = 10 .^round.(range(log10(minimum(Y2)),log10(maximum(Y2)),5), digits=2)
-yticks = round.(range(log10(minimum(Y2)),log10(maximum(Y2)),5), digits=2)
 
-# plot!(p, tlin, Ylin, color=palette[3], label="", yticks=ceil(minimum(Ylin)) ; inset=bbox(0.6, 0.4, 0.4, 0.4), subplot=2)
-plot!(p, tlist, Y2, color=palette[3], label="", yticks=yticks ; inset=bbox(0.6, 0.4, 0.4, 0.4), subplot=2)
-savefig(p, "test.svg");
+println("---------------------")
---
+++ MEsolve(LINDBLAD)/harm_lindblad_single.jl
@@ -0,0 +1,97 @@
+ENV["GKSwstype"] = "100"
+using QuantumToolbox, Plots, LaTeXStrings, Printf, Base.Threads
+theme(:dao)
+palette = theme_palette(:dao)
+
+function N_BE(x)
+    if x == -1
+        return 0 # χ→+∞ ≡ T→0 ⇒ N_th→0
+    else
+        return 1/(exp(x)-1)
+    end
+end
+
+println("---------------------")
+
+    # Simulation Parameters
+# ===========================================
+α0 = 2.0 # Starting level for the initial coherent state
+χ = 1
+Nth = N_BE(χ) # mean thermal photon number in the cavity 
+tlist = range(0,10,1000)
+ω_k = 1e3 # unitless frequency system oscillation / coupling ratio (weak coupling regime if ω_k>>1)
+
+
+Ncut = Nth + 4*√(Nth*(1+Nth)) + abs(α0)^2+4*abs(α0) # Dimension cutoff in the operators: μ+4σ of thermal state (steady state) + μ+4σ of initial coherent state
+
+prct = 100 # POURCENTAGE DE TRONCATURE EN PLUS
+factor = 1.0+prct/100
+Ncut = Int(ceil(Ncut*factor)) # We take +prct% of the maximal population and round it to the upper value
+# println("⚠ Nth≈$(Int(ceil(Nth))) => Ncut=$Ncut")
+
+# Operators
+a = destroy(Ncut)
+
+c_decay = √(1+Nth)*a
+c_excite = √(Nth)*a'
+c_ops = [c_decay, c_excite]
+
+ψ0 = coherent(Ncut, α0)
+
+n_op = a'*a
+e_ops=[n_op]
+H = ω_k*a'*a 
+
+sol = mesolve(H, ψ0, tlist, c_ops; e_ops=e_ops, progress_bar=Val(true)) # solver
+ρf = sol.states[end]
+pop_sim = real.(diag(ρf))
+
+function pop_th(level,χ)
+    if χ==-1 && level==0
+        return 1.0
+    elseif χ==-1 && level!=0
+        return 0.0
+    else
+        return (1-exp(-χ))*exp(-level*χ)
+    end
+end
+
+Levels = 0:Ncut-1
+pop_thv = pop_th.(Levels,χ)
+yticksval = range(minimum(pop_sim),maximum(pop_sim), 7)
+ytickslab = ["$(round(v*100, digits=2))%" for v in yticksval]
+ytickslab = ["$(round(v, sigdigits=1))" for v in yticksval]
+
+pop = plot(Levels, pop_sim, label="pop sim", lw=5, title="Population of each level after the evolution\n"*L"\sum pop_{th}\approx %$(round(sum(pop_thv), digits=5))\qquad \sum pop_{sim}\approx %$(round(sum(pop_sim), digits=10))", xlabel="Levels", ylabel="Population", yticks=(yticksval, ytickslab), legend=:bottomleft)
+plot!(pop, Levels, pop_thv, label="pop th", ls=:dash)
+plot!(pop, Levels, abs.(pop_thv.-pop_sim), color=palette[3], label="", title="Absolute error", lw=2 ; inset=bbox(0.5, 0.2, 0.4, 0.4), subplot=2)
+savefig(pop, "populations$(ω_k).svg")
+
+n_anal(t) = α0^2*exp(-t)+Nth*(1-exp(-t)) # analytical solution
+N = n_anal.(tlist)
+Y = real.(sol.expect[1,:])
+Y2 = abs.(N.-Y)
+
+p = plot(title=L"ħω=%$(χ)\times k_{B}T \qquad N_{cut}=%$(Ncut)"*"(+$(prct)%)", legend=:outerright)
+# Graph of the population
+plot!(p, tlist, Y,label="Simulation",lw=3, color=palette[1]);
+plot!(p, tlist, N, lw=2, label="Analytical model", color=palette[2], ls=:dash);
+xlabel!(p, L"$\tau=k t$ (unitless)")
+ylabel!(p, L"\langle \hat{n}(\tau)\rangle")
+
+# Error graph (inset inside the previous graph)
+yticks_val = range(minimum(Y2),maximum(Y2),5)
+yticks_label = [@sprintf("%.1e", v) for v in yticks_val]
+yticks = (yticks_val, yticks_label)
+
+plot!(p, tlist, Y2, color=palette[3], label="", yticks=yticks, title="Absolute error", lw=2 ; inset=bbox(0.3, 0.4, 0.4, 0.4), subplot=2)
+savefig(p, "$(prct)%$(ω_k).svg")
+
+P_coh(n,a) = exp(-abs(a)^2)*abs(a)^(2n)/factorial(big(n))
+yticksv = 10.0 .^(.-(1:6))
+
+distrib = plot(Levels, pop_thv, title="Population distribution (log scale on the y-scale)", xlabel=L"n", ylabel=L"\mathbb{P}(n)", label="Thermal", legend=:topright, z_order=:front, lw=3, yscale=:log10, yticks=:native, ylims=(1e-7,0.1), ls=:dash)
+plot!(distrib, Levels, pop_sim, label="Simulation population", z_order=:back, lw=3)
+savefig(distrib, "distrib$(ω_k).svg")
+
+println("---------------------")
