83603a1552c8c0d7a3de59637d60bcd36d38c94b
JulioSteve <jules.stevenot25@gmail.com>
2026-06-05 09:31:30 +0200

    up

--- MCWF(PHOTODET)/Qubit2.jl
+++ MCWF(PHOTODET)/Qubit2.jl
@@ -65,7 +65,11 @@ function sim(Ntraj::Integer)
     Traveraged = zeros(Real, length(τlist))
     Jumpalltraj = zeros(Int, length(τlist))
     ρeeaveraged = zeros(Real, length(τlist))
-    for _ in 1:Ntraj
+    for k in 1:Ntraj
+        prog = round(k*100/Ntraj, digits=2)
+        print("\r Calculating... $(prog)%")
+        flush(stdout)
+
         Res_single = single_traj()
         Traveraged += Res_single[1]
         ρeeaveraged += Res_single[2]
@@ -92,14 +96,14 @@ function plotting(Ntraj::Integer, simres)
 
         savefig(Psingle, "single.svg")
     else
-        Pav = plot()
-        
+        Pav = plot(xlabel=L"$\tau=kt$ (unitless)", ylabel="Expected value of averaged observables", legend=:outerright)
         plot!(Pav, τlist, ρeeaveraged, label=L"\mathbb{E}\left[\rho_{ee}(\tau)\right]=\mathbb{E}[\langle\sigma_+\sigma_-\rangle_{\psi}(\tau)]", color=palette[1], lw=5)
         plot!(Pav, τlist, Traveraged, label=L"\mathbb{E}\left[\mathrm{Tr}\;[\breve{\rho}\,(\tau)]\right]=\mathbb{E}\left[\langle\breve{\psi}\;\, |\breve{\psi}\,\rangle(\tau)\right]", ls=:dash, color=palette[2], lw=3)
         plot!(Pav, τlist, ρee.(τlist), label=L"\rho_{ee}^\mathrm{th}(\tau)", ls=:dot, color=palette[3], lw=2)
         savefig(Pav, "$(Ntraj)trajs.svg")
-        
-        Histo = histogram(Jumpalltraj, bins=200, normalize=:pdf, legend=:outerright, xlabel=L"$\tau = kt$ (unitless)", ylabel="Quantum jump distribution", extra_kwargs=ekw, label="")
+      
+        Histo = bar(τlist, Jumpalltraj./Ntraj, legend=:outerright, xlabel=L"$\tau = kt$ (unitless)", ylabel="Quantum jump distribution", extra_kwargs=ekw, label="Jump distribution", lw=0)
+        # plot!(Histo, τlist, ,label="Theoretical model")
         savefig(Histo, "histo.svg")
     end
 
@@ -124,6 +128,6 @@ end
 
 ####################################
 
-Ntraj = 100_000
+Ntraj = 100
 RES = sim(Ntraj)
 plotting(Ntraj, RES)
